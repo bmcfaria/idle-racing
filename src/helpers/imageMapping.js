e513dae8d253e18f2d2b6ee74707f411b33ada2c
Bruno Faria <brunomcfaria@gmail.com>
2020-05-01 16:02:10 +0100

    Smaller tmp images

--- src/helpers/imageMapping.js
+++ src/helpers/imageMapping.js
@@ -21,20 +21,20 @@ export const carImages = {
 
 export const trackImages = {
   'c92fb1c7-2902-426c-b479-90d29c933e12':
-    'https://images.unsplash.com/photo-1578401449027-acdcfd8137e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1789&q=80',
+    'https://images.unsplash.com/photo-1578401449027-acdcfd8137e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=310',
   '54d3522d-22c2-4c95-b9d1-68c7d6853b90':
-    'https://images.unsplash.com/photo-1553618737-1785e21374f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1947&q=80',
+    'https://images.unsplash.com/photo-1553618737-1785e21374f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=310',
   '8ecb7e5d-e6bb-464f-89b9-9839e58bc7a1':
-    'https://images.unsplash.com/photo-1584635790312-8ce625d7f53d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1584635790312-8ce625d7f53d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=310',
   'b300263e-a7f5-4c24-a7f9-6bcd6e8e624f':
-    'https://images.unsplash.com/photo-1527757780101-05985993b2e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80',
+    'https://images.unsplash.com/photo-1527757780101-05985993b2e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=310',
 
   'cd1e3e65-9e95-44d5-bbeb-75b910648594':
-    'https://images.unsplash.com/flagged/photo-1550755832-1dd26fd6bb0e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1978&q=80',
+    'https://images.unsplash.com/flagged/photo-1550755832-1dd26fd6bb0e?ixlib=rb-1.2.1&auto=format&fit=crop&w=310',
   '9d624db2-e0b8-441f-a6a7-5ec0b6b8287d':
-    'https://images.unsplash.com/photo-1513526595084-81bab56e821f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80',
+    'https://images.unsplash.com/photo-1513526595084-81bab56e821f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=310',
   'fedebe4a-6eab-4cfe-aa27-bd6afe3f19c5':
-    'https://images.unsplash.com/photo-1559406041-c7d2b2e98690?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80',
+    'https://images.unsplash.com/photo-1559406041-c7d2b2e98690?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=310',
 };
 
 export const imageMapping = {
