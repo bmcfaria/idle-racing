47179e6b9189e03590c6e88a5610e61923feca59
Bruno Faria <brunomcfaria@gmail.com>
2020-09-07 17:01:45 +0100

    More dev reset helper

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -14,45 +14,67 @@ const Settings = () => {
     dispatch(resetAction);
   };
 
-  const rich = () => {
-    dispatch(
-      resetDevAction({
-        money: 9999999999999999999,
-      })
-    );
+  const resetDev = state => {
+    dispatch(resetDevAction(state));
   };
 
   return (
     <Box>
       <Flex
         w="240px"
-        h="80px"
+        padding="16px"
         borderRadius="16px"
         direction="column"
         bg={colors.lightGray}
         alignItems="center"
       >
-        <Text h="30px" lineHeight="30px" textAlign="center" fontSize="18px">
+        <Text lineHeight="18px" textAlign="center" fontSize="18px">
           Delete save data
         </Text>
-        <Button marginTop="4px" bg={colors.red} onClick={reset}>
+        <Button marginTop="8px" bg={colors.red} onClick={reset}>
           Reset
         </Button>
       </Flex>
       {inDev && (
         <Flex
           w="240px"
-          h="80px"
+          padding="16px"
           borderRadius="16px"
           marginTop="16px"
           direction="column"
           bg={colors.lightGray}
           alignItems="center"
         >
-          <Text h="30px" lineHeight="30px" textAlign="center" fontSize="18px">
+          <Text lineHeight="18px" textAlign="center" fontSize="18px">
             Rich
           </Text>
-          <Button marginTop="4px" bg={colors.red} onClick={rich}>
+          <Button
+            marginTop="8px"
+            bg={colors.red}
+            onClick={() => resetDev({ money: 9999999999999999999 })}
+          >
+            Reset
+          </Button>
+        </Flex>
+      )}
+      {inDev && (
+        <Flex
+          w="240px"
+          padding="16px"
+          borderRadius="16px"
+          marginTop="16px"
+          direction="column"
+          bg={colors.lightGray}
+          alignItems="center"
+        >
+          <Text lineHeight="18px" textAlign="center" fontSize="18px">
+            Enough for car and lvl1 upgrades
+          </Text>
+          <Button
+            marginTop="8px"
+            bg={colors.red}
+            onClick={() => resetDev({ money: 722 })}
+          >
             Reset
           </Button>
         </Flex>
