195f124320974033b2bfdefbaa8bf01adae90b5d
Bruno Faria <brunomcfaria@gmail.com>
2020-09-16 11:27:38 +0100

    Remove empty sponsors from Race page

--- src/components/Race.js
+++ src/components/Race.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import { Box, Flex } from '@chakra-ui/core';
 import { useDynamicCardContainerWidth } from '../helpers/hooks';
-import CollapsiblePanel from './CollapsiblePanel';
 import { BottomSpacer } from './BottomSpacer';
 import CardRaceEvent from './CardRaceEvent';
 import { useSelector } from 'react-redux';
 import { raceEventsSelector } from '../state/selectors';
-
-const Sponsors = props => (
-  <CollapsiblePanel text="Sponsors (TBD)" {...props}></CollapsiblePanel>
-);
 
 const Race = props => {
   const events = useSelector(raceEventsSelector);
@@ -17,15 +12,12 @@ const Race = props => {
 
   return (
     <Box {...props}>
-      <Sponsors w={`${containerWidth - 16}px`} />
-
       <Flex
         wrap="wrap"
         margin="0 auto"
         paddingLeft={`${containerWidth > 0 ? 16 : 0}px`}
         boxSizing="content-box"
         w={`${containerWidth || 320}px`}
-        marginTop="24px"
       >
         {events.map((event, index) => (
           <Box marginRight="16px" marginBottom="16px" key={index}>
