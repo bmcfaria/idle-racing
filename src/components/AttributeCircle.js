1f9cd488ecdf1ce1735644b1a19a7d7c2e02d329
Bruno Faria <brunomcfaria@gmail.com>
2020-09-30 16:32:50 +0100

    Extracted upgrade circle logic from attribute

--- src/components/AttributeCircle.js
+++ src/components/AttributeCircle.js
@@ -1,119 +1,50 @@
 import React from 'react';
 import { Flex, Text } from '@chakra-ui/core';
 import { colors } from '../helpers/theme';
-
-const baseDashArray = (base, dashSize, divider, perimeter) => {
-  const size = dashSize - divider;
-  const arrayString = [...Array(base - 1)].reduce(
-    result => `${result} ${divider} ${size}`,
-    `${size}`
-  );
-  return `${arrayString} ${perimeter - size * base - divider * (base - 1)}`;
-};
+import UpgradableCircle from './UpgradableCircle';
 
 const AttributeCircle = ({ attr, text, nextUpgrade, showMax, ...props }) => {
-  const size = 44;
   const totalNumber = attr.max + attr.base;
-  const divider = 2;
-  const radius = size / 2;
-  const perimeter = 2 * Math.PI * radius;
-
-  const dashSize = perimeter / totalNumber;
 
   const base = attr.base;
   const upgrades = attr.upgrade;
-
-  const baseSize = (dashSize - divider) * base + divider * (base - 1);
-  const baseAndUpgradeSize =
-    (dashSize - divider) * (base + upgrades) + divider * (base + upgrades - 1);
 
   const showNextUpgradeValue =
     nextUpgrade && attr.value + 1 <= attr.max + attr.base;
 
   return (
-    <Flex w={size + 8} h={size + 8} position="relative" {...props}>
-      <svg width={size + 8} height={size + 8}>
-        <circle
-          r={radius}
-          cx="50%"
-          cy="50%"
-          strokeWidth="3"
-          stroke={colors.white}
-          fill="none"
-          strokeDasharray={`${dashSize - divider} ${divider}`}
-          strokeDashoffset={`${perimeter / 4 - divider / 2}`}
-        />
-        <circle
-          r={radius}
-          cx="50%"
-          cy="50%"
-          strokeWidth="5"
-          stroke="black"
-          fill="none"
-          strokeDasharray={baseDashArray(base, dashSize, divider, perimeter)}
-          strokeDashoffset={`${perimeter / 4 - divider / 2}`}
-        />
-        {upgrades && (
-          <circle
-            r={radius}
-            cx="50%"
-            cy="50%"
-            strokeWidth="3"
-            stroke={colors.darkGray}
-            fill="none"
-            strokeDasharray={baseDashArray(
-              upgrades,
-              dashSize,
-              divider,
-              perimeter
-            )}
-            strokeDashoffset={`${
-              perimeter / 4 - divider / 2 - baseSize - divider
-            }`}
-          />
-        )}
-        {showNextUpgradeValue && (
-          <circle
-            r={radius}
-            cx="50%"
-            cy="50%"
-            strokeWidth="3"
-            stroke={colors.blue}
-            fill="none"
-            strokeDasharray={baseDashArray(1, dashSize, divider, perimeter)}
-            strokeDashoffset={`${
-              perimeter / 4 - divider / 2 - baseAndUpgradeSize - divider
-            }`}
-          />
-        )}
-      </svg>
-      <Flex w="100%" h="100%" position="absolute">
-        <Flex margin="auto" direction="column" alignItems="center">
-          <Text fontSize="12px" lineHeight="12px" color={colors.darkGray}>
-            {text}
+    <UpgradableCircle
+      base={base}
+      value={base + upgrades}
+      max={totalNumber}
+      showNextUpgradeValue={showNextUpgradeValue}
+      {...props}
+    >
+      <Flex margin="auto" direction="column" alignItems="center">
+        <Text fontSize="12px" lineHeight="12px" color={colors.darkGray}>
+          {text}
+        </Text>
+        <Flex alignItems="baseline">
+          <Text
+            fontSize="14px"
+            lineHeight="14px"
+            color={showNextUpgradeValue ? colors.purple : 'inherit'}
+          >
+            {showNextUpgradeValue ? attr.value + 1 : attr.value}
           </Text>
-          <Flex alignItems="baseline">
+          {showMax && (
             <Text
-              fontSize="14px"
-              lineHeight="14px"
-              color={showNextUpgradeValue ? colors.purple : 'inherit'}
+              marginTop="auto"
+              fontSize="10px"
+              lineHeight="10px"
+              color={colors.darkGray}
             >
-              {showNextUpgradeValue ? attr.value + 1 : attr.value}
+              /{attr.max + attr.base}
             </Text>
-            {showMax && (
-              <Text
-                marginTop="auto"
-                fontSize="10px"
-                lineHeight="10px"
-                color={colors.darkGray}
-              >
-                /{attr.max + attr.base}
-              </Text>
-            )}
-          </Flex>
+          )}
         </Flex>
       </Flex>
-    </Flex>
+    </UpgradableCircle>
   );
 };
 
---
+++ src/components/UpgradableCircle.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Flex } from '@chakra-ui/core';
+import { colors } from '../helpers/theme';
+
+const baseDashArray = (base, dashSize, divider, perimeter) => {
+  const size = dashSize - divider;
+  const arrayString = [...Array(base - 1)].reduce(
+    result => `${result} ${divider} ${size}`,
+    `${size}`
+  );
+  return `${arrayString} ${perimeter - size * base - divider * (base - 1)}`;
+};
+
+const UpgradableCircle = ({
+  base = 0,
+  value = 0,
+  max = 1,
+  size = 44,
+  showNextUpgradeValue,
+  children,
+  ...props
+}) => {
+  const divider = 2;
+  const radius = size / 2;
+  const perimeter = 2 * Math.PI * radius;
+
+  const dashSize = perimeter / max;
+
+  const baseSize = (dashSize - divider) * base + divider * (base - 1);
+  const baseAndUpgradeSize =
+    (dashSize - divider) * value + divider * (value - 1);
+
+  const upgrade = value - base;
+
+  return (
+    <Flex w={size + 8} h={size + 8} position="relative" {...props}>
+      <svg width={size + 8} height={size + 8}>
+        {/* All circle segments */}
+        <circle
+          r={radius}
+          cx="50%"
+          cy="50%"
+          strokeWidth="3"
+          stroke={colors.white}
+          fill="none"
+          strokeDasharray={`${dashSize - divider} ${divider}`}
+          strokeDashoffset={`${perimeter / 4 - divider / 2}`}
+        />
+        {/* Base / "initial value" segments */}
+        {base > 0 && (
+          <circle
+            r={radius}
+            cx="50%"
+            cy="50%"
+            strokeWidth="5"
+            stroke="black"
+            fill="none"
+            strokeDasharray={baseDashArray(base, dashSize, divider, perimeter)}
+            strokeDashoffset={`${perimeter / 4 - divider / 2}`}
+          />
+        )}
+        {/* Already upgraded segments */}
+        {upgrade > 0 && (
+          <circle
+            r={radius}
+            cx="50%"
+            cy="50%"
+            strokeWidth="3"
+            stroke={colors.darkGray}
+            fill="none"
+            strokeDasharray={baseDashArray(
+              upgrade,
+              dashSize,
+              divider,
+              perimeter
+            )}
+            strokeDashoffset={`${
+              perimeter / 4 - divider / 2 - baseSize - divider
+            }`}
+          />
+        )}
+        {/* Next segment highlight */}
+        {showNextUpgradeValue && (
+          <circle
+            r={radius}
+            cx="50%"
+            cy="50%"
+            strokeWidth="3"
+            stroke={colors.blue}
+            fill="none"
+            strokeDasharray={baseDashArray(1, dashSize, divider, perimeter)}
+            strokeDashoffset={`${
+              perimeter / 4 - divider / 2 - baseAndUpgradeSize - divider
+            }`}
+          />
+        )}
+      </svg>
+      {children && (
+        <Flex w="100%" h="100%" position="absolute">
+          {children}
+        </Flex>
+      )}
+    </Flex>
+  );
+};
+
+export default UpgradableCircle;
