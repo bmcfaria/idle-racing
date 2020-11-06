469db14b50535bf29f2704fd2c75da881cd83751
Bruno Faria <brunomcfaria@gmail.com>
2020-11-06 18:39:52 +0000

    Race track svg animateMotion

--- src/components/StopRaceModal.js
+++ src/components/StopRaceModal.js
@@ -1,20 +1,61 @@
-import React from 'react';
-import { Text, Flex } from '@chakra-ui/core';
+import React, { useEffect, useState } from 'react';
+import { Text, Flex, Box } from '@chakra-ui/core';
 import { colors } from '../helpers/theme';
 import Modal from './Modal';
 import Button from './Button';
 import { useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { stopRaceAction } from '../state/actions';
-import { useRace } from '../helpers/hooksRace';
+import { useRace, useTrack } from '../helpers/hooksRace';
 import { useHistoryHelper } from '../helpers/hooks';
+import getImageTrack from '../helpers/imageMappingTracks';
 
 const StopRaceModal = props => {
   const location = useLocation();
   const history = useHistoryHelper();
   const raceId = location.state?.cancelRaceModal;
   const race = useRace(raceId);
+  const track = useTrack(race?.track);
   const dispatch = useDispatch();
+  const [imageContainerRef, setImageContainerRef] = useState();
+
+  useEffect(() => {
+    if (imageContainerRef) {
+      const path = imageContainerRef
+        ?.getElementsByTagName('path')?.[0]
+        ?.getAttribute('d');
+
+      let circleEl = document.createElementNS(
+        'http://www.w3.org/2000/svg',
+        'circle'
+      );
+      circleEl.setAttribute('r', 5);
+      circleEl.setAttribute('fill', colors.blue);
+
+      let animateMotionEl = document.createElementNS(
+        'http://www.w3.org/2000/svg',
+        'animateMotion'
+      );
+      animateMotionEl.setAttribute('dur', '5s');
+      animateMotionEl.setAttribute('repeatCount', 'indefinite');
+      animateMotionEl.setAttribute('path', path);
+
+      circleEl.appendChild(animateMotionEl);
+      imageContainerRef.appendChild(circleEl);
+
+      // Minor zoom out so it won't cut the animated circle
+      const originalViewBox = imageContainerRef
+        .getAttribute('viewBox')
+        .split(' ');
+
+      imageContainerRef.setAttribute(
+        'viewBox',
+        `${originalViewBox[0]} ${parseInt(originalViewBox[1]) - 2} ${
+          originalViewBox[2]
+        } ${parseInt(originalViewBox[3]) + 4}`
+      );
+    }
+  }, [imageContainerRef]);
 
   const stopRace = () => {
     dispatch(stopRaceAction(raceId));
@@ -34,7 +75,17 @@ const StopRaceModal = props => {
         borderRadius="16px"
         {...props}
       >
-        <Text textAlign="center" fontSize="16px">
+        {track && (
+          <Box
+            ref={ref => setImageContainerRef(ref)}
+            w="auto"
+            h="52px"
+            color={colors.darkGray}
+            as={getImageTrack(track)}
+          />
+        )}
+
+        <Text textAlign="center" fontSize="16px" marginTop="16px">
           {race?.auto ? 'Stop auto race' : 'Stop race'}
         </Text>
         <Flex marginTop="16px">
